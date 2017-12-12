bbe6f5822e39aa61868ba2e8f2188ff1f78eb7e5
dafinoer <dafidzeko@gmail.com>
2017-12-12 23:51:51 +0700

    add checkbox and editText

--- app/src/main/java/com/dafinoer/javadroid/MainActivity.java
+++ app/src/main/java/com/dafinoer/javadroid/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,8 +31,29 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textNumber);
         int getValue = Integer.parseInt(textView.getText().toString());
         int tot = getValue * 1000;
+
+        String setToppingName = "Not Use Add Topping";
+        CheckBox checkBox = (CheckBox) findViewById(R.id.ice_cone);
+        CheckBox checBoxWhiteCreamer = (CheckBox) findViewById(R.id.white_creamer);
+        EditText editText = (EditText) findViewById(R.id.input_name);
+
+        if (checkBox.isChecked()){
+            tot += 1000;
+            setToppingName = "User Topping Cone";
+        }
+        if(checBoxWhiteCreamer.isChecked()) {
+            tot += 500;
+            setToppingName = "Use Topping White Cream";
+        }
+
+        if(checkBox.isChecked() && checBoxWhiteCreamer.isChecked()){
+            setToppingName = "topping cone and white cream";
+        }
+
         rupiahConvert.setMoney(tot);
-        setPrice(rupiahConvert.getConvert());
+        setPrice(" hi thanks "+editText.getText()+"\n " +
+                setToppingName + "\n"+
+                "total price "+rupiahConvert.getConvert());
     }
 
     private void displayText(int number) {
@@ -39,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setPrice(String jmlah) {
-        TextView textView = (TextView) findViewById(R.id.price);
+        TextView textView = (TextView) findViewById(R.id.order_summary_tex_view);
 //        int getTot = jmlah * 2000;
         textView.setText(jmlah);
     }
@@ -72,5 +95,6 @@ public class MainActivity extends AppCompatActivity {
             Log.e("erorr", e.getMessage());
         }
     }
+
 
 }
